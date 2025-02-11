16c8e5bc3575073b8c481016eaa24263dce030bc
Chakirtkbr <chakritsixtyfour@gmail.com>
2025-02-11 14:18:11 +0700

    basic setup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
-import express, { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import app from './app';
 
-const app = express();
-const prisma = new PrismaClient();
-const port = 8080;
-
-app.get('/', async (req: Request, res: Response) => {
-  // ตัวอย่างการดึงข้อมูลจากฐานข้อมูล
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port} 1231`);
+  console.log(`Server is running on port ${port}`);
 });
